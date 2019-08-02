bb4c7f716eab8c069ff0fbe2a04f3488782910f4
Flavio Perez <flavio.perez@ipointsystems.com>
2019-08-02 10:45:48 -0500

    Removed integration tests and added unit testing. #2

---
+++ NaturalEventMsTests/FunctionTest.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture.Kernel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TraceWriter = Microsoft.Azure.WebJobs.Host.TraceWriter;
+
+namespace FunctionTestHelper
+{
+    public abstract class FunctionTest
+    {
+        //protected TraceWriter log;        
+
+        public HttpRequest HttpRequestSetup(Dictionary<String, StringValues> query, string body)
+        {
+            var reqMock = new Mock<HttpRequest>();
+
+            reqMock.Setup(req => req.Query).Returns(new QueryCollection(query));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(body);
+            writer.Flush();
+            stream.Position = 0;
+            reqMock.Setup(req => req.Body).Returns(stream);
+            return reqMock.Object;
+        }
+    }
+
+    public class AsyncCollector<T> : IAsyncCollector<T>
+    {
+        public readonly List<T> Items = new List<T>();
+
+        public Task AddAsync(T item, CancellationToken cancellationToken = default)
+        {
+
+            Items.Add(item);
+
+            return Task.FromResult(true);
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(true);
+        }
+    }
+
+    //public class VerboseDiagnosticsTraceWriter : TraceWriter
+    //{
+
+    //    public VerboseDiagnosticsTraceWriter() : base(TraceLevel.Verbose)
+    //    {
+
+    //    }
+    //    public override void Trace(TraceEvent traceEvent)
+    //    {
+    //        Debug.WriteLine(traceEvent.Message);
+    //    }
+    //}
+}
